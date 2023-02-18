197c76c5c11dad518a700fcacda428e1ee75f639
Visual Pivert <visualpivert@gmail.com>
2023-02-18 15:05:29 +0300

    define style and radial-gradient

--- src/JbCard.tsx
+++ src/JbCard.tsx
@@ -1,15 +1,38 @@
-import { Children, PropsWithChildren } from "react";
+import { Children, MouseEventHandler, PropsWithChildren, useCallback, useState } from "react";
 
 type JbCardType = PropsWithChildren<{
     header? : string
 }>
 
 export function JbCard ({header, children} : JbCardType) {
-    return <div className="jbcard">
+    const [position, setPosition] = useState({x: 0, y:0})
+    const [isRadial, setRadial] = useState(false)
+
+    const handleMovedMouse = useCallback(function (event: any) {
+        setPosition({x: event.clientX, y: event.clientY})
+    }, [])
+
+    const handleEnteredMouse = useCallback(function () {
+        setRadial(true)
+    }, [])
+
+    const handleLeavedMouse = useCallback(function () {
+        setRadial(false)
+    }, [])
+    const radial_gradient = (isRadial) 
+        ? {background: `radial-gradient(150px 150px at ${position.x}px ${position.y}px, rgb(221, 7, 7) 0%, rgb(0, 0, 0) 100%)`}
+        : {background: `rgb(0, 0, 0)`}
+    return <div 
+        className="jbcard" 
+        style={radial_gradient}
+        onMouseMove={handleMovedMouse}
+        onMouseEnter={handleEnteredMouse}
+        onMouseLeave={handleLeavedMouse}
+    >
         <div className="jbcard__header">
             <a href="#">{header}</a>
         </div>
-        <div className="jbcard__main">
+        <div className="jbcard__main" >
             <p>{children}</p>
         </div>
     </div>
